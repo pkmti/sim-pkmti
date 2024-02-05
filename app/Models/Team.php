9cef69d773b13196095df679f859ddd92398f548
adityamahendrap <ptadityamahendrap@gmail.com>
2024-02-06 00:22:17 +0800

    add: models

---
+++ app/Models/Team.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $protected = [];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function proposal()
+    {
+        return $this->hasOne(Proposal::class);
+    }
+}

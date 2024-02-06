ec4b0b4c4e625c8064525fdd1c49046f1849a595
rammm2005 <ditarama985@gmail.com>
2024-02-06 14:45:43 +0700

    feat(landing-page): add dark mode toogle

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,4 +1,5 @@
 import { Link, Head } from '@inertiajs/react';
+import DarkMode from '@/Components/darkMode';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
@@ -14,26 +15,23 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                     <ul className="menu menu-horizontal px-1">
                         <li>
                             <a href='#'>Home</a>
-                        </li>  
+                        </li>
                         <li>
                             <a href='#'>About Us</a>
-                        </li>  
+                        </li>
                         <li>
                             <a href='#'>FAQ</a>
-                        </li>  
+                        </li>
                         <li>
                             <a href='#'>Contact Us</a>
-                        </li>  
+                        </li>
                     </ul>
                 </div>
                 <div className="navbar-end space-x-4">
-                    <div className="toggle-theme bg-primary p-2 rounded-full cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                        </svg>
-                    </div>
 
-                {auth.user ? (
+                    <DarkMode/>
+                    
+                    {auth.user ? (
                         <Link
                             href={route('dashboard')}
                             className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
